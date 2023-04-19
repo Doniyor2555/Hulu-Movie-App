b91740c299f9f5dd58e0cea3bb7847332dba7155
DoniyorShodiyevDoni <109288985+Doniyor2555@users.noreply.github.com>
2023-04-19 16:43:51 +0500

    Hulu-Movie-App

---
+++ components/Header.js
@@ -0,0 +1,31 @@
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from '@heroicons/react/outline';
+
+import Image from "next/image"
+
+import HeaderItem from "./HeaderItem"
+
+function Header() {
+  return (
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+      <div className='flex flex-grow justify-evenly max-w-2xl'>
+        <HeaderItem title='Home' Icon={HomeIcon} />
+        <HeaderItem title='TRENDING' Icon={LightningBoltIcon} />
+        <HeaderItem title='VERIFIED' Icon={BadgeCheckIcon} />
+        <HeaderItem title='COLLECTIONS' Icon={CollectionIcon} />
+        <HeaderItem title='SEARCH' Icon={SearchIcon} />
+        <HeaderItem title='ACCOUNT' Icon={UserIcon} />
+      </div>
+
+      <Image className="object-contain" src='https://links.papareact.com/ua6' width={200} height={100} alt='logo'/>
+    </header>
+  )
+}
+
+export default Header

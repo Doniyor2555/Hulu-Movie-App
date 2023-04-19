366be701e4395aa12f3269f75f74028b3c0e8133
DoniyorShodiyevDoni <109288985+Doniyor2555@users.noreply.github.com>
2023-04-19 19:52:02 +0500

    React-Sneakers

---
+++ components/Thumbnail.js
@@ -0,0 +1,19 @@
+import Image from "next/image"
+
+function Thumbnail({ result }) {
+  const BASE_URL = 'https://image.tmdb.org/t/p/original/';
+
+  return (
+    <div>
+      <Image layout="responsive"
+        src={
+          `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
+          `${BASE_URL}${result.poster_path}`}
+        width={1080}
+        height={1920} />
+    </div>
+  )
+}
+
+export default Thumbnail
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,29 @@
-import { Inter } from 'next/font/google'
 import Header from '../../components/Header'
 import Nav from '../../components/Nav'
+import Results from '../../components/Results'
+import requests from '../../utils/requests'
 
-const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home({ results }) {
   return (
     <div>
 
       <Header />
 
-      {/* Nav */}
-      <Nav/>
+      <Nav />
+      <Results results={results}/>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(`https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchingTrending.url}`).then(res => res.json())
+
+  return {
+    props: {
+      results: request.results
+    }
+  }
 }
